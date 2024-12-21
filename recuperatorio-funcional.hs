2942353499efc6fe6d74bea135a3abc31afa77a9
LuichiTW <ltitaewatanabe@frba.utn.edu.ar>
2024-12-21 09:43:34 -0300

    punto 1

--- recuperatorio-funcional.hs
+++ recuperatorio-funcional.hs
@@ -1,1 +1,22 @@
 import Text.Show.Functions
+
+--Punto 1
+data Aventurero = UnAventurero {
+    nombre :: String,
+    carga :: Int,
+    salud :: Int,
+    tieneCoraje :: Bool,
+    criterioSeleccionEncuentros :: Criterio
+}
+
+type Encuentro = Aventurero -> Aventurero
+type Criterio = Aventurero -> Bool
+
+conformista :: Criterio
+conformista unAventurero = True
+
+valiente :: Criterio
+valiente unAventurero = tieneCoraje unAventurero || salud unAventurero > 50
+
+lightPacker :: Int -> Criterio
+lightPacker umbral unAventurero = (<umbral).carga $ unAventurero 
