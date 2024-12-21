608599a7e44fd42452e443c0e5612adb1dc4696d
LuichiTW <ltitaewatanabe@frba.utn.edu.ar>
2024-12-21 10:45:59 -0300

    avances punto 3

--- recuperatorio-funcional.hs
+++ recuperatorio-funcional.hs
@@ -31,9 +31,27 @@ tieneMasCantidadLetras cantidad unAventurero = (>cantidad).length.nombre $ unAve
 joaquin = UnAventurero "joaqi" 10 20 True conformista
 
 sumarParesCarga :: [Aventurero] -> Int
-sumarParesCarga unosAventureros = sum.obtenerCargasPares $ unosAventureros
+sumarParesCarga unosAventureros = sum.cargasPares $ unosAventureros
 
-obtenerCargasPares :: [Aventurero] -> [Int]
-obtenerCargasPares unosAventureros = filter even.map carga $ unosAventureros
+cargasPares :: [Aventurero] -> [Int]
+cargasPares unosAventureros = filter even.map carga $ unosAventureros
 
-type Encuentro = Aventurero -> Aventurero
+--Punto 3
+type Encuentro = Personaje -> Aventurero -> Aventurero
+data Personaje =
+    Curandero
+    | Inspirador
+    | Embaucador
+
+encuentroPersonaje :: Encuentro
+encuentroPersonaje personaje unAventurero = efectoPersonaje personaje.modificarCarga (-) (1) $  unAventurero
+
+modificarCarga :: (Int -> Int -> Int) -> Int -> Aventurero -> Aventurero
+modificarCarga modificador numero unAventurero = unAventurero {carga = modificador (carga unAventurero) numero}
+
+efectoPersonaje :: Personaje -> Aventurero -> Aventurero
+efectoPersonaje Curandero unAventurero = modificarCarga (div) 2 unAventurero
+efectoPersonaje Curandero unAventurero = modificarCarga (div) 2 unAventurero
+
+aumentoSaludPorcentual :: Int -> Aventurero -> Aventurero
+aumentoSaludPorcentual porcentaje unAventurero = unAventurero {div porcentaje 100
